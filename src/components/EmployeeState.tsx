9f76baed88e4018c04d06b0e172867b5acf68b49
terza98 <terza98@hotmail.rs>
2021-07-22 20:02:00 +0200

    minify code

--- src/components/EmployeeState.tsx
+++ src/components/EmployeeState.tsx
@@ -37,46 +37,17 @@ export default function EmployeeState({ id, state }: Employee) {
 
   return (
     <Flex>
-      <Box
-        {...boxStyles}
-        bg={activeState === STATES.ADDED ? "#1D5AD4" : ""}
-        color={activeState === STATES.ADDED ? "white" : ""}
-        onClick={() => handleStateSelect(STATES.ADDED)}
-      >
-        <Text {...textStyles}>{STATES.ADDED}</Text>
-      </Box>
-      <Box
-        {...boxStyles}
-        bg={activeState === STATES.IN_CHECK ? "#1D5AD4" : ""}
-        color={activeState === STATES.IN_CHECK ? "white" : ""}
-        onClick={() => handleStateSelect(STATES.IN_CHECK)}
-      >
-        <Text {...textStyles}>{STATES.IN_CHECK}</Text>
-      </Box>
-      <Box
-        {...boxStyles}
-        bg={activeState === STATES.APPROVED ? "#1D5AD4" : ""}
-        color={activeState === STATES.APPROVED ? "white" : ""}
-        onClick={() => handleStateSelect(STATES.APPROVED)}
-      >
-        <Text {...textStyles}>{STATES.APPROVED}</Text>
-      </Box>
-      <Box
-        {...boxStyles}
-        bg={activeState === STATES.ACTIVE ? "#1D5AD4" : ""}
-        color={activeState === STATES.ACTIVE ? "white" : ""}
-        onClick={() => handleStateSelect(STATES.ACTIVE)}
-      >
-        <Text {...textStyles}>{STATES.ACTIVE}</Text>
-      </Box>
-      <Box
-        {...boxStyles}
-        bg={activeState === STATES.INACTIVE ? "#1D5AD4" : ""}
-        color={activeState === STATES.INACTIVE ? "white" : ""}
-        onClick={() => handleStateSelect(STATES.INACTIVE)}
-      >
-        <Text {...textStyles}>{STATES.INACTIVE}</Text>
-      </Box>
+      {Object.keys(STATES).map((key) => (
+        <Box
+          {...boxStyles}
+          key={key}
+          bg={activeState === key ? "#1D5AD4" : ""}
+          color={activeState === key ? "white" : ""}
+          onClick={() => handleStateSelect(key)}
+        >
+          <Text {...textStyles}>{key}</Text>
+        </Box>
+      ))}
     </Flex>
   );
 }
